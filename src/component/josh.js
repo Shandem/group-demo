b7673b8eafa4900ff3d512865433e8290490c3cd
unknown <gutierrezjoshuah@gmail.com>
2022-07-05 09:49:24 -0700

    tester

---
+++ src/component/josh.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Josh = () => {
+    return (
+        <div>
+            <div>
+                <a href='https://wac-cdn.atlassian.com/dam/jcr:c6db91c1-1343-4d45-8c93-bdba910b9506/02%20Branch-1%20kopiera.png?cdnVersion=418'/>
+                        more words
+            </div>
+        </div>
+    );
+}
+
+export default Josh;
---
+++ src/pages/josh.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Josh2 from '../component/josh'
+
+const Josh = () => {
+    return (
+        <div>
+            Hello banana
+            <Josh2/>
+        </div>
+    );
+}
+
+export default Josh;
